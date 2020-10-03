0df35204f23e303e8cb3ff09a9f7139ad7d5480a
UDAY PRATAP SINGH <47354743+Coderu2058@users.noreply.github.com>
2020-10-03 20:17:08 +0530

    Added the optimised prime algorithm

--- Algorithms/prime/Optimised_prime.cpp
+++ Algorithms/prime/Optimised_prime.cpp
@@ -1,1 +1,33 @@
-#add your code here
+void SieveOfEratosthenes(int n) 
+{ 
+   //boolean array to store the true and false values for being a prime number.
+    bool prime[n+1]; 
+    memset(prime, true, sizeof(prime)); 
+  
+    for (int p=2; p*p<=n; p++) 
+    { 
+        // If prime[p] is not changed, then it is a prime 
+        if (prime[p] == true) 
+        {  
+            for (int i=p*p; i<=n; i += p) 
+                prime[i] = false; 
+        } 
+    } 
+  
+    // Print all prime numbers 
+    for (int p=2; p<=n; p++) 
+       if (prime[p]) 
+          cout << p << " "; 
+} 
+  
+// Main function 
+int main() 
+{ 
+    int n;
+    cout<<" Enter a number : ";
+    cin>>n;
+    cout << "Following are the prime numbers smaller "
+         << " than or equal to " << n << endl; 
+    SieveOfEratosthenes(n); 
+    return 0; 
+} 
